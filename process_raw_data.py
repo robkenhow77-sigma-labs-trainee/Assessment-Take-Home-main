9c2af3b9a1eab1d5a8b21a707cb3afbf37c372e2
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-01-10 15:58:50 +0000

    challenge 1

--- process_raw_data.py
+++ process_raw_data.py
@@ -1,6 +1,7 @@
 """A script to process book data."""
 from argparse import ArgumentParser
 import csv 
+import sqlite3
 
 
 # Initialise
@@ -23,13 +24,18 @@ def get_csv_data(file_name: str, folder: str='data') -> list[dict]:
         return [row for row in rows]
 
 
+def get_authors_names(filename:str="authors.db", folder:str="data") -> list[tuple]:
+    """Query database for author_id and author_name"""
+    conn = sqlite3.connect(f'{folder}/{filename}')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM author"  )
+    authors = cur.fetchall()
+    cur.close()
+    conn.close()
+    return authors
+    
+
 # Transform
-
-def get_authors_names():
-    """Query database for map"""
-    pass
-
-
 def get_relevant_data(data: list[str]) -> list[dict]:
     """Gets only the relevant columns."""
     relevant_data = []
@@ -55,51 +61,82 @@ def filter_for_author_title_missing_data(data: list[dict]) -> list[dict]:
     return filtered_data
             
 
-def clean_title(data: list[dict]): #ADD TYPE
+def clean_title(data: list[dict]) -> list[dict]:
+    """Cleans the book title to the appropriate format."""
     for row in data:
         title = row["title"]
         if "(" in title:
-            row["title"] = title.split('(')[0].strip()
+            title = title.split('(')[0].strip()
+        row["title"] = title.replace('"', '')
     return data
 
 
-def clean_author_id(data: list[dict]): #ADD TYPE
+def clean_author_id(data: list[dict]) -> list[dict]:
+    """Cleans the author_id to the appropriate format."""
     for row in data:
         row["author_id"] = int(float(row["author_id"]))
     return data
 
 
-def clean_year(data: list[dict]): #ADD TYPE
+def clean_year(data: list[dict]) -> list[dict]:
+    """Cleans the book year to the appropriate format."""
     for row in data:
-        row["author_id"] = int(row["year"])
+        row["year"] = int(row["year"])
     return data
 
 
-def clean_rating(data: list[dict]): #ADD TYPE
+def clean_rating(data: list[dict]) -> list[dict]:
+    """Cleans the book rating to the appropriate format."""
     for row in data:
         row["rating"] = float(row["rating"].replace(",", "."))
     return data
  
 
-def clean_ratings(data: list[dict]): #ADD TYPE
+def clean_ratings(data: list[dict]) -> list[dict]:
+    """Cleans the book ratings to the appropriate format."""
     for row in data:
         row["ratings"] = int(row["ratings"].replace("`", ""))
     return data
 
 
-# Create csv
-def create_csv(data: list[dict], filename: str, folder: str='data') -> None:
-    """Creates a csv."""
-    with 
+def map_author_id(data: list[dict], authors: list[tuple]) -> list[dict]:
+    """Creates a mapping dicitonary in the form -> id: name
+    Then maps the author_id to author_name.
+    Adds author_name key and removes authour_id """
+    mapping = {str(author[0]): author[1] for author in authors}
+    for row in data:
+        author_id = row["author_id"]
+        author_name = mapping[str(author_id)]
+        row["author_name"] = author_name
+        row.pop("author_id")
+    return data
 
+
+def order_rows_by_rating(data:list[dict]) -> list[dict]:
+    """Orders the rows based on rating."""
+    return sorted(data, key=lambda row: row['rating'], reverse=True)
+
+
+# Create csv
+def create_csv(data: list[dict], filename: str='PROCESSED_DATA.csv', folder: str='data') -> None:
+    """Creates a csv."""
+    with open(f"{folder}/{filename}", mode='w', newline='', encoding="UTF-8") as new_csv:
+        writer = csv.writer(new_csv)
+        # Write Header
+        writer.writerow(["title", "author_name", "year", "rating", "ratings"])
+        # Write rows
+        for row in data:
+            values = [row["title"], row["author_name"], row["year"], row["rating"], row["ratings"]] 
+            writer.writerow(values) 
 
 
 if __name__ == "__main__":
     # Initialise
     filename = get_cli_args()
 
-    # Get csv data
+    # Get csv and author data
     data = get_csv_data(filename)
+    authors = get_authors_names()
 
     # Clean data
     data = get_relevant_data(data)
@@ -109,9 +146,11 @@ if __name__ == "__main__":
     data = clean_year(data)
     data = clean_rating(data)
     data = clean_ratings(data)
+    data = map_author_id(data, authors)
+    data = order_rows_by_rating(data)
 
-
-
-    for i in range(10):
-        print(data[i])
     # Create csv
+    create_csv(data)
+
+
+    # with open()
