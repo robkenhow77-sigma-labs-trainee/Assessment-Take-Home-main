fc7346824527863ab1eca3852dbe204c4bbd75cb
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-01-10 14:59:54 +0000

    assessment

--- process_raw_data.py
+++ process_raw_data.py
@@ -25,37 +25,72 @@ def get_csv_data(file_name: str, folder: str='data') -> list[dict]:
 
 # Transform
 
-def get_authors_names()
-
-
-def check_title(data: list[dict]): #ADD TYPE
+def get_authors_names():
+    """Query database for map"""
     pass
 
 
-def check_author_name(data: list[dict]): #ADD TYPE
-    pass
+def get_relevant_data(data: list[str]) -> list[dict]:
+    """Gets only the relevant columns."""
+    relevant_data = []
+    for row in data:
+      relevant_data.append({
+            "title": row["book_title"],
+            "author_id": row["author_id"],
+            "year": row["Year released"],
+            "rating": row["Rating"],
+            "ratings": row["ratings"]
+      })
+    return relevant_data
 
 
-def check_year(data: list[dict]): #ADD TYPE
-    pass
-
-
-def check_rating(data: list[dict]): #ADD TYPE
-    pass
-
-def check_ratings(data: list[dict]): #ADD TYPE
-    pass
-
-
-def filter_for_errors(data: list[dict]) -> list[dict]:
+def filter_for_author_title_missing_data(data: list[dict]) -> list[dict]:
     """Check each row for valid data."""
-    pass
+    filtered_data = []
+    for row in data:
+        title = row["title"]
+        author_id = row["author_id"]
+        if title != '' and author_id != '':
+            filtered_data.append(row)
+    return filtered_data
+            
+
+def clean_title(data: list[dict]): #ADD TYPE
+    for row in data:
+        title = row["title"]
+        if "(" in title:
+            row["title"] = title.split('(')[0].strip()
+    return data
+
+
+def clean_author_id(data: list[dict]): #ADD TYPE
+    for row in data:
+        row["author_id"] = int(float(row["author_id"]))
+    return data
+
+
+def clean_year(data: list[dict]): #ADD TYPE
+    for row in data:
+        row["author_id"] = int(row["year"])
+    return data
+
+
+def clean_rating(data: list[dict]): #ADD TYPE
+    for row in data:
+        row["rating"] = float(row["rating"].replace(",", "."))
+    return data
+ 
+
+def clean_ratings(data: list[dict]): #ADD TYPE
+    for row in data:
+        row["ratings"] = int(row["ratings"].replace("`", ""))
+    return data
 
 
 # Create csv
 def create_csv(data: list[dict], filename: str, folder: str='data') -> None:
     """Creates a csv."""
-    pass
+    with 
 
 
 
@@ -64,8 +99,19 @@ if __name__ == "__main__":
     filename = get_cli_args()
 
     # Get csv data
-    csv_data = get_csv_data(filename)
+    data = get_csv_data(filename)
 
     # Clean data
+    data = get_relevant_data(data)
+    data = filter_for_author_title_missing_data(data)
+    data = clean_title(data)
+    data = clean_author_id(data)
+    data = clean_year(data)
+    data = clean_rating(data)
+    data = clean_ratings(data)
 
+
+
+    for i in range(10):
+        print(data[i])
     # Create csv
